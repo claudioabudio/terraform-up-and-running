4f049153e53156472b49e0149af5c958fdc625ea
claudio Abud <clau@gmail.com>
2023-09-24 23:11:24 -0500

    use s3 as backend for the terraform state

--- main.tf
+++ main.tf
@@ -4,24 +4,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0261755bbcb8c4a84"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.example_instance_sg.id]
-  tags = {
-    Name = "terraform-example"
-  }
-
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  user_data_replace_on_change = true
-}
-
 
 resource "aws_security_group" "example_instance_sg" {
   name = "terraform-example-instance"
